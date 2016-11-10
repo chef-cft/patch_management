340ec0969bbfac8ffbb52284bf19a943dcc76132
Nathan Cerny <ncerny@gmail.com>
2016-11-10 13:01:54 -0600

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# patch_management
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'patch_management'
+maintainer 'Chef Software, Inc'
+maintainer_email 'success@chef.io'
+license 'apachev2'
+description 'Installs/Configures patch_management'
+long_description 'Installs/Configures patch_management'
+version '0.1.0'
